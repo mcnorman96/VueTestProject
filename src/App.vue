6302fe050158bf7716bf59b06ba395a00848fff1
Marcus Norman <marcus-norman@hotmail.com>
2024-07-07 21:19:42 +0200

    Fix to menu styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-const menuItemStyle = 'text-xl text-gray-500 block hover:text-white flex flex-col justify-center text-center text-xs items-center p-8';
+const menuItemStyle = 'border-l-2 border-blue-950 text-xl text-gray-500 block hover:text-white flex flex-col justify-center text-center text-xs items-center p-8';
 </script>
 
 <template>
@@ -8,16 +8,16 @@ const menuItemStyle = 'text-xl text-gray-500 block hover:text-white flex flex-co
     <header class="min-h-screen w-32 bg-blue-950 ">
       <div class="wrapper">
         <nav>
-          <RouterLink activeClass="text-white border-l-2" :class="menuItemStyle" to="/">
+          <RouterLink activeClass="text-white border-l-white" :class="menuItemStyle" to="/">
             <font-awesome-icon icon="fa-solid fa-solid fa-house" class="h-8 mb-2"/> 
             Home
           </RouterLink>
-          <RouterLink activeClass="text-white border-l-2" :class="menuItemStyle" to="/users">
+          <RouterLink activeClass="text-white border-l-white" :class="menuItemStyle" to="/users">
             <font-awesome-icon icon="fa-solid fa-user-group" class="h-8 mb-2 "/> 
             Users
           </RouterLink>
-          <RouterLink activeClass="text-white border-l-2" :class="menuItemStyle" to="/products">
-            <font-awesome-icon icon="fa-solid fa-shirt" class="h-8 mb-2 flex-c "/> 
+          <RouterLink activeClass="text-white border-l-white" :class="menuItemStyle" to="/products">
+            <font-awesome-icon icon="fa-solid fa-shirt" class="h-8 mb-2 "/> 
             Products
           </RouterLink>
         </nav>
